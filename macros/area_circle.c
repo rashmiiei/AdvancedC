5bfc74aca48514ec73183ebdb379267a24141583
Rashmi <reach.rashmi9@gmail.com>
2025-08-15 16:54:29 +0530

    Add files via upload

---
+++ macros/area_circle.c
@@ -0,0 +1,19 @@
+/* Write a macro to define the constant value of PI as 3.14159. Find the area of the circle. */
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main()
+{
+    float radius, area;
+    printf("enter the value of the radius : ");
+    scanf("%f", &radius);
+    
+    area = (PI * radius * radius);
+    printf("Area of the circle is %g.\n", area);
+    
+    return 0;
+    
+    
+    
+}
