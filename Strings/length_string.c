cc86d81d48b44a147cb36b3f127bd91c674bdedf
Rashmi <reach.rashmi9@gmail.com>
2025-08-16 14:06:28 +0530

    Update length_string.c

--- Strings/length_string.c
+++ Strings/length_string.c
@@ -11,14 +11,15 @@ int main()
     printf("Enter the string : ");
     scanf("%19[^\n]", str); //using selective scanf
     
-    while(str[i] != '\0') //iterating rhrough the string till I reach the null character
+    while(str[i] != '\0') //iterating through the string till I reach the null character
     {
         i++;
         len++;
     }
     
-    printf("Lenth of the string is %d.\n", len);
+    printf("Length of the string is %d.\n", len);
     
     return 0;
     
+
 }
