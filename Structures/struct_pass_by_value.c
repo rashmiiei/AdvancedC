a7070395f432d585d4f1a82f24f01312a23e8c21
Rashmi <reach.rashmi9@gmail.com>
2025-08-30 12:41:41 +0530

    Add files via upload

---
+++ Structures/struct_pass_by_value.c
@@ -0,0 +1,27 @@
+include <stdio.h>
+struct Student
+{
+    int id;
+    char name[20];
+    char address[60];
+};
+
+//we are not returning any value,hence data type is void
+//collecting all 84 bytes into another structure 
+// Changes made here won't affect the original structure in main
+void data (struct Student s)
+{
+    s.id = 10; // Assign 10 to the id field (only modifies the copy)
+};
+
+int main()
+{
+    struct Student s1;
+    
+    // function call, pass by value method, changes won't reflect in s1
+    //we are not collecting it into any variable 
+    data(s1); 
+    printf("%d\n", s1.id); // garbage value will be printed
+
+    return 0;
+}
