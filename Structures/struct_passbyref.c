ab1142f868a9e411a276e82fd097943209280374
Rashmi <reach.rashmi9@gmail.com>
2025-08-30 17:23:06 +0530

    Add files via upload

---
+++ Structures/struct_passbyref.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct Student // defining the structure variable
+{
+    int id;
+    char name[20];
+    char address[60];
+
+};
+
+// return type is void because we ae not returning anything
+// we are collecting the structure address in a structure pointer sptr 
+void data (struct Student *sptr) //function definition
+{
+    sptr->id = 10; //using the arrow operator to access the structure field
+    
+};
+
+int main()
+{
+    struct Student s1;
+    
+    // pass by reference method of function call
+    // Sending the address of structure to another function
+    data(&s1); 
+    
+    printf("%d\n", s1.id);
+    
+    return 0;
+}
