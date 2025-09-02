c8fcf56428bfe3a9db4fd7acda0e3453a6af0bbe
Rashmi <reach.rashmi9@gmail.com>
2025-09-02 16:51:01 +0530

    Update 2D_array_print.c

--- 2D_arrays/2D_array_print.c
+++ 2D_arrays/2D_array_print.c
@@ -14,9 +14,13 @@ int main()
 		// Loop through each column in the current row
 		for(cols = 0; cols < 3; cols++)
 		{
-			printf("%d\t", arr[rows][cols]);
+			//printf("%d\t", arr[rows][cols]);
+
+			// arr[i][j]; Assume arr[i] is x; x[j] -> *(x+j) -> *(a[i]+j)
+			printf("%d\t", *(arr[rows] + cols)); 
 		}
 		printf("\n");
 	}
 	return 0;
+
 }
