da5d4024e0324610dc3748eb79df958bf6cb600c
Rashmi <reach.rashmi9@gmail.com>
2026-01-01 23:48:31 +0530

    Add files via upload

---
+++ BasicRefreshers/neon_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int num, square, sum=0, rem, original_num;
+    printf("enter a number : ");
+    scanf("%d", &num);
+
+    original_num = num;
+    square = num*num;
+
+    while(square != 0)
+    {
+        rem = square % 10;
+        sum += rem;
+        square = square/10;
+    }
+
+    if(sum == original_num)
+    {
+        printf("neon number \n");
+    }
+    else
+    {
+        printf("not a neon number \n");
+    }
+    return 0;
+}
