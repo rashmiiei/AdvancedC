46ed025a45675bf079e70a8431be1f13e0a548e6
Rashmi <reach.rashmi9@gmail.com>
2026-07-07 16:47:53 +0530

    Add files via upload

---
+++ Advanced Functions/command line arguments/print_all_cla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    //for(int i=0; i<argc; i++)
+    for(int i=0; argv[i] != NULL; i++)
+    {
+        printf("%s \n", argv[i]);
+    }
+
+    return 0;
+}
